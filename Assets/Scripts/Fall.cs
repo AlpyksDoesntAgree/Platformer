9bdf519a9b30ecaca359ee94648a70ef73afac3c
AlpyksDoesntAgree <UnityAlpyks@gmail.com>
2025-04-12 14:36:44 +0300

    added falling logic and health

--- Assets/Scripts/Fall.cs
+++ Assets/Scripts/Fall.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Fall : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform player;
+    private Rigidbody2D playerRb;
+    private PlayerController playerController;
+    private bool damaged = false;
+    [SerializeField] private Vector3 checkPoint;
+    [SerializeField] private int minY;
+    [SerializeField] private float _speed;
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        playerRb = GameObject.Find("Player").GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (player.position.y <= minY)
+        {
+            if (!damaged)
+            { playerController.Damage(1); damaged = !damaged;  _speed = 2f; playerController.isMovingEnabled = false; StartCoroutine(Falling()); }
+        }
+    }
+
+    IEnumerator Falling()
+    {
+        playerRb.bodyType = RigidbodyType2D.Kinematic;
+        playerRb.velocity = Vector3.zero;
+
+        while (Vector3.Distance(player.position, checkPoint) > 1f)
+        {
+            player.position = Vector3.Lerp(player.position, checkPoint, _speed * Time.deltaTime);
+            yield return null;
+        }
+        player.position = checkPoint;
+
+        if(player.position == checkPoint)
+        {
+            playerController.isMovingEnabled = true;
+            playerRb.bodyType = RigidbodyType2D.Dynamic;
+
+            yield return new WaitForSeconds(0.5f);
+            damaged = !damaged;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    //UI
+    [SerializeField] private GameObject heart;
+    [SerializeField] private Transform playerUI;
+    private int _health = 3;
+
+    //PlayerControl
     private Rigidbody2D rb;
     private float speed = 5.0f;
     [SerializeField] private Animator animator;
@@ -14,14 +21,15 @@ public class PlayerController : MonoBehaviour
 
     private CameraController cam;
 
-    private int _health = 3;
     [HideInInspector] public bool doubleJump;
     private bool jumped = false;
 
     [HideInInspector] public string nameScene;
+    [HideInInspector] public bool isMovingEnabled = true;
 
     void Start()
     {
+        updateUI();
         nameScene = PlayerPrefs.GetString("NameScene", "StarterLvl");
         doubleJump = PlayerPrefs.GetInt("HasDoubleJump", 0) == 1;
         cam = GameObject.Find("Main Camera").GetComponent<CameraController>();
@@ -33,8 +41,11 @@ public class PlayerController : MonoBehaviour
         float move = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
 
-        if (move > 0) { transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); cam.offset = new Vector3(-0.1f, 0, 0); }
-        if (move < 0) { transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); cam.offset = new Vector3(0.1f, 0, 0); }
+        if (isMovingEnabled)
+        {
+            if (move > 0) { transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); cam.offset = new Vector3(-0.1f, 0, 0); }
+            if (move < 0) { transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); cam.offset = new Vector3(0.1f, 0, 0); }
+        }
 
         //isGrounded
         isGrounded = Physics2D.OverlapCircle(transform.position, 0.4f, groundLayer);
@@ -75,6 +86,18 @@ public class PlayerController : MonoBehaviour
     public void Damage(int i)
     {
         _health -= i;
-        Debug.Log(_health);
+        updateUI();
+    }
+    private void updateUI()
+    {
+        foreach (Transform child in playerUI.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < _health; i++)
+        {
+            Instantiate(heart, new Vector3(playerUI.position.x + 1.15f * i, playerUI.position.y), Quaternion.identity, playerUI);
+        }
     }
 }
