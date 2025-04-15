0f120729f75f0e868480e4e57ba9386fa7b2c128
AlpyksDoesntAgree <UnityAlpyks@gmail.com>
2025-04-15 09:22:20 +0300

    evacuation

---
+++ Assets/Scripts/Traps/Sawblade.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sawblade : MonoBehaviour
+{
+    [SerializeField] private float _distance;
+    [SerializeField] private float _speed = 0.012f;
+
+    private Transform block;
+    private bool movingRight = true;
+    private Vector3 startPos;
+
+    private Transform player;
+    private PlayerController playerCon;
+    void Start()
+    {
+        block = GetComponent<Transform>();
+
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        playerCon = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        startPos = block.position;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0,0, 280f * Time.deltaTime);
+
+        if (block.position.x >= startPos.x + _distance)
+            movingRight = false;
+        else if (block.position.x <= startPos.x)
+            movingRight = true;
+
+        if (movingRight)
+            block.position = Vector3.MoveTowards(block.position, new Vector3(startPos.x + _distance, startPos.y, startPos.z), _speed);
+        else
+            block.position = Vector3.MoveTowards(block.position, new Vector3(startPos.x - _distance, startPos.y, startPos.z), _speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            playerCon.Damage(1);
+    }
+}
